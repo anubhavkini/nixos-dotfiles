813c91f6d205245c0cf68d8dca3d8a6d4be5bcda
Anubhav Kini <anubhavkini@pm.me>
2021-09-16 19:43:02 +0530

    Add git signing

--- home/modules/vcs/git/default.nix
+++ home/modules/vcs/git/default.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.git = {
     enable = true;
   	userName = "Anubhav Kini";
   	userEmail = "anubhavkini@pm.me";
+    signing = {
+      gpgPath = "${config.programs.gpg.package}/bin/gpg";
+      key = "CB79992D";
+      signByDefault = true;
+    };
   };
 }
