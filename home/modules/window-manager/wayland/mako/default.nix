f17c4a20d30bc359e8916c3710d646c6dfb0d44b
Anubhav Kini <anubhavkini@pm.me>
2021-09-24 16:58:58 +0530

    New battery

--- home/modules/window-manager/wayland/mako/default.nix
+++ home/modules/window-manager/wayland/mako/default.nix
@@ -4,17 +4,12 @@
   programs.mako = {
     enable = true;
     anchor = "top-center";
-    backgroundColor = "#0F5151FF";
-    borderSize = 0;
-    font = "source code pro 11";
     iconPath = "~/.nix-profile/share/icons/hicolor";
-    margin = null;
-    markup = false;
-    maxIconSize = 16;
-    progressColor = "source #0F5151CC";
-    textColor = "#FFFFFFFF";
     extraConfig = ''
       text-alignment=center
+
+      [mode=silent]
+      invisible=1
     '';
   };
 
