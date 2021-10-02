ec5feaad7931cd7e9d00c561633e0e128f2f398c
Anubhav Kini <anubhavkini@pm.me>
2021-10-02 19:53:53 +0530

    Configure mpv

--- home/modules/media/mpv/default.nix
+++ home/modules/media/mpv/default.nix
@@ -7,6 +7,18 @@
     config = {
       # Hardware acceleration.
       hwdec = "auto";
+      profile = "gpu-hq";
+
+      # Better audio at fast speeds.
+      af = "scaletempo2";
+
+      # eng by default.
+      alang = "eng";
+      slang = "eng";
+      vlang = "eng";
+
+      # Subtitles.
+      sub-font-size = "35";
     };
   };
 }
